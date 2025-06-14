03a3473c84fb50db3557cb065e7d863f82e02a62
rohan2RS <guptarohan30112005@gmail.com>
2025-06-14 11:42:52 +0530

    loan eligiblty app

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load model
+model = pickle.load(open('loan_prediction_model.pkl', 'rb'))
+
+st.title("Loan Approval Prediction App")
+
+# Input fields
+gender = st.selectbox("Gender", ['Male', 'Female'])
+married = st.selectbox("Married", ['Yes', 'No'])
+dependents = st.selectbox("Dependents", ['0', '1', '2', '3+'])
+education = st.selectbox("Education", ['Graduate', 'Not Graduate'])
+self_employed = st.selectbox("Self Employed", ['Yes', 'No'])
+applicant_income = st.number_input("Applicant Income")
+coapplicant_income = st.number_input("Coapplicant Income")
+loan_amount = st.number_input("Loan Amount")
+loan_term = st.selectbox("Loan Term", [360.0, 120.0, 180.0, 240.0])
+credit_history = st.selectbox("Credit History", [1.0, 0.0])
+property_area = st.selectbox("Property Area", ['Urban', 'Rural', 'Semiurban'])
+
+# Convert inputs to model format
+input_data = [
+    0 if gender == 'Female' else 1,
+    1 if married == 'Yes' else 0,
+    {'0': 0, '1': 1, '2': 2, '3+': 3}[dependents],
+    0 if education == 'Graduate' else 1,
+    1 if self_employed == 'Yes' else 0,
+    applicant_income,
+    coapplicant_income,
+    loan_amount,
+    loan_term,
+    credit_history,
+    {'Urban': 2, 'Rural': 0, 'Semiurban': 1}[property_area]
+]
+
+if st.button("Predict Loan Approval"):
+    prediction = model.predict([input_data])[0]
+    if prediction == 1:
+        st.success(" Loan Approved")
+    else:
+        st.error(" Loan Not Approved")
